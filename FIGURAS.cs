c574e9ff888468b089e09e1e1b1cbde445a41eca
Ingrid Centeno <grymarcelita@gmail.com>
2021-02-16 22:23:57 +0100

    Agregar archivos de proyecto.

---
+++ FIGURAS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIGURAS_V1
+{
+    class FIGURAS
+    {
+        // public decimal area
+        //{ get; set;} //get  y set son de tipo lectura y escritura
+        double area = 0;
+        double lado = 0;
+        double ancho = 0;
+        double radio = 0;
+
+        public void areaCuadrado()  //sintaxis: visibilidad seguido de tipo de valor a retornar, seguido del nombre de la funcion
+        {
+            Console.WriteLine("Escribe el lado: ");
+          lado = Convert.ToDouble(Console.ReadLine());
+            area = lado * lado;
+            Console.WriteLine("El resultado del area es: " + area);
+
+        }
+        public void areaRectangulo() 
+        {
+            Console.WriteLine("Escribe el largo: ");
+            lado = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Escribe el ancho: ");
+            ancho = Convert.ToDouble(Console.ReadLine());
+            area = lado * ancho;
+            Console.WriteLine("El resultado del area es: " + area);
+        }
+        public void areaCirculo()
+        {
+            Console.WriteLine("Escribe el radio: ");
+            radio = Convert.ToDouble(Console.ReadLine());
+            area = Math.PI*Math.Pow(radio,2);
+            Console.WriteLine("El resultado del area es: " + area);
+        }
+
+
+    }
+}
+
+//**************************Ingrid Centeno**************************
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace FIGURAS_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n = 1;
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("¿Desea utilizar la aplicación para el cálculo del área de alguna figura? <S/N>");
+                   string valor =  Console.ReadLine();
+                   // if (valor=="S" || valor =="s")
+                   if(valor.ToUpper()=="S")
+                    {
+                        Console.WriteLine("Elige la figura del área que desea calcular: \n" +
+                                        "1. AREA DE UN CUADRADO \n" + "2. AREA DE UN RECTANGULO \n" + "3. AREA DE UN CIRCULO");
+                        string opcion = Console.ReadLine();
+                        FIGURAS fIGURAS = new FIGURAS(); //instanciando a la clase figuras
+                        switch (opcion)
+                        {
+                            case "1": fIGURAS.areaCuadrado(); break;
+                            case "2": fIGURAS.areaRectangulo(); break;
+                            case "3": fIGURAS.areaCirculo(); break;
+                            default:
+                                Console.WriteLine("OPCION ELEGIDA NO VALIDA");
+                                break;
+                        }
+                        n++;
+                    }
+                    else if(valor == "N" || valor== "n" ) //otra forma de comparar con el "or" "||"
+                    {
+                        return; //retorna a la función principal y se termina la ejecución en curso
+                    }
+
+                    
+                }
+                
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("La aplicación ha fallado. Contacte con su técnico. " + ex.Message);
+               // throw;
+            }
+            
+                
+        }
+    }
+}
